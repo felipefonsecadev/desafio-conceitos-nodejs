af519912d833b2f11166d00f6b4c471ca8261ecd
Fipas <fonsecafel@gmail.com>
2020-07-10 00:28:25 -0300

    Fulfill test specs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
-// const { uuid } = require("uuidv4");
+function validadeRepositoryId(request, response, next) {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository ID not found' });
+  }
+
+  request.repositoryIndex = repositoryIndex;
+  return next();
+}
 
 const app = express();
 
@@ -11,23 +23,51 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validadeRepositoryId, (request, response) => {
+  const { title, url, techs } = request.body;
+  const { repositoryIndex } = request;
+
+  const oldRepository = repositories[repositoryIndex];
+  const newRepository = {
+    id: oldRepository.id,
+    title,
+    url,
+    techs,
+    likes: oldRepository.likes
+  }
+
+  repositories[repositoryIndex] = newRepository;
+  return response.json(newRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validadeRepositoryId, (request, response) => {
+  const { repositoryIndex } = request;
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validadeRepositoryId, (request, response) => {
+  const { repositoryIndex } = request;
+  repositories[repositoryIndex].likes = repositories[repositoryIndex].likes + 1;
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
